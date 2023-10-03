bae1f680e3c58867532cd9801c592734d14f18e6
rolfst <rolfst@gmail.com>
2023-10-03 14:41:36 +0200

    feat(i18n): be more specific for localization

--- default.nix
+++ default.nix
@@ -87,8 +87,15 @@ in {
   i18n = {
     defaultLocale = mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = "nl_NL.UTF-8";
+      LC_ADDRESS = "nl_NL.UTF-8";
+      LC_IDENTIFICATION = "nl_NL.UTF-8";
+      LC_MEASUREMENT = "nl_NL.UTF-8";
       LC_MONETARY = "nl_NL.UTF-8";
+      LC_NAME = "nl_NL.UTF-8";
+      LC_NUMERIC = "nl_NL.UTF-8";
+      LC_PAPER = "nl_NL.UTF-8";
+      LC_TELEPHONE = "nl_NL.UTF-8";
+      LC_TIME = "nl_NL.UTF-8";
     };
   };
 
