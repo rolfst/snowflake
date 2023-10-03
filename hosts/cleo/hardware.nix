f89f0ee2663daf4efe318de426da769157f650c3
rolfst <rolfst@gmail.com>
2023-10-03 14:59:23 +0200

    chore: fix hd id and graphic card

--- hosts/cleo/hardware.nix
+++ hosts/cleo/hardware.nix
@@ -6,29 +6,32 @@
   ...
 }: let
   inherit (lib) mkDefault;
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-GO
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
 in {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
+    device = "/dev/disk/by-uuid/238f6eb4-b155-499e-b75a-2f1d233797ed";
     fsType = "ext4";
-    options = ["noatime" "x-gvfs-hide"];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-label/home";
-    fsType = "ext4";
-    neededForBoot = true;
-    options = ["noatime" "x-gvfs-hide"];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-label/BOOT";
+    device = "/dev/disk/by-uuid/0F59-178A";
     fsType = "vfat";
-    options = ["x-gvfs-hide"];
   };
 
-  swapDevices = [{device = "/dev/disk/by-label/swap";}];
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/3c6e053d-68ca-423e-8ff8-184dab3eab02";
+    fsType = "ext4";
+  };
+
+  swapDevices = [{device = "/dev/disk/by-uuid/e02f5046-315f-4f6e-a748-a843336fabf2";}];
 
   boot = {
     initrd = {
@@ -67,56 +70,64 @@ in {
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = true;
+    open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
     prime = {
-		# Make sure to use the correct Bus ID values for your system!
-		intelBusId = "PCI:0:2:0";
-		nvidiaBusId = "PCI:1:0:0";
-	};
+      offload.enable = true;
+      # Make sure to use the correct Bus ID values for your system!
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   powerManagement.cpuFreqGovernor = mkDefault "schedutil";
+  environment.systemPackages = [nvidia-offload];
 
   # Manage device power-control:
   services = {
+    upower.enable = true;
     power-profiles-daemon.enable = true;
     thermald.enable = true;
+    xserver = {
+      videoDrivers = ["nvidia"];
+      deviceSection = ''
+        Option "TearFree" "true"
+      '';
+      libinput = {
+        enable = true;
+        touchpad = {
+          accelSpeed = "0.5";
+          accelProfile = "adaptive";
+          disableWhileTyping = true;
+          naturalScrolling = true;
+          scrollMethod = "twofinger";
+          tapping = true;
+        };
+      };
+    };
   };
 
   # Finally, our beloved hardware module(s):
   modules.hardware = {
     pipewire = {
       enable = true;
-      lowLatency.enable = true;
+      # lowLatency.enable = true;
     };
     bluetooth.enable = true;
     # kmonad.deviceID = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
     pointer.enable = true;
     printer.enable = true;
     razer.enable = true;
-  };
-
-  services = {
-    upower.enable = true;
-
-    xserver = {
-      videoDrivers = ["nvidia"];
-      libinput.touchpad = {
-        accelSpeed = "0.5";
-        accelProfile = "adaptive";
-      };
-    };
   };
 }
