a213c208636cf8791a6a037c1c8f243f0e289097
rolfst <rolfst@gmail.com>
2023-10-05 16:41:27 +0200

    feat(aws): add cli control

--- modules/develop/default.nix
+++ modules/develop/default.nix
@@ -1,11 +1,21 @@
-{ config, options, lib, pkgs, ... }:
-
-let inherit (lib.modules) mkIf;
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.attrsets) attrValues;
 in {
-  options.modules.develop = let inherit (lib.options) mkEnableOption;
-  in { xdg.enable = mkEnableOption "XDG-related conf" // { default = true; }; };
+  options.modules.develop = let
+    inherit (lib.options) mkEnableOption;
+  in {xdg.enable = mkEnableOption "XDG-related conf" // {default = true;};};
 
   config = mkIf config.modules.develop.xdg.enable {
     # TODO:
+    user.packages = attrValues {
+      inherit (pkgs) awscli2;
+    };
   };
 }
