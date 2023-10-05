43055aa6485c516ba9ccf710a61b3d59dae926ea
rolfst <rolfst@gmail.com>
2023-10-05 16:40:46 +0200

    feat(scanner): add scanner functionality

--- modules/hardware/printer.nix
+++ modules/hardware/printer.nix
@@ -1,16 +1,27 @@
-{ config, options, lib, pkgs, ... }:
-
-let inherit (lib.modules) mkIf;
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
 in {
-  options.modules.hardware.printer = let inherit (lib.options) mkEnableOption;
-  in { enable = mkEnableOption "printer support"; };
+  options.modules.hardware.printer = let
+    inherit (lib.options) mkEnableOption;
+  in {enable = mkEnableOption "printer support";};
 
   config = mkIf config.modules.hardware.printer.enable {
     services.printing = {
       enable = true;
-      drivers = [ pkgs.hplip pkgs.xsane ];
+      drivers = [pkgs.hplip pkgs.xsane];
     };
+    user.packages = [pkgs.xsane];
 
+    hardware.sane = {
+      enable = true;
+      extraBackends = [pkgs.hplipWithPlugin];
+    };
     # Enable wireless access to printers
     services.avahi = {
       enable = true;
