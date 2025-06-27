6f75bc46f59a63a7c4d94d09c80b8ef569dee555
rolfst <rolfst@gmail.com>
2025-06-27 15:56:20 +0200

    chore: add mise module

--- modules/desktop/editors/libreoffice.nix
+++ modules/desktop/editors/libreoffice.nix
@@ -7,15 +7,34 @@
 }: let
   inherit (lib.modules) mkIf;
   inherit (lib.attrsets) attrValues optionalAttrs;
+  inherit (lib) mkDictFromLibreofficeGit;
 in {
   options.modules.desktop.editors.libreoffice = let
     inherit (lib.options) mkEnableOption;
   in {enable = mkEnableOption "post-modern office suit";};
 
+  # let
+  #   nl-nl = mkDictFromLibreofficeGit {
+  #     subdir = "nl";
+  #     shortName = "nl-nl";
+  #     shortDescription = "Nederlands (Nederland)";
+  #     dictFileName = "nl_NL";
+  #     readmeFileName = "nl";
+  #   };
+  # in {
   config = mkIf config.modules.desktop.editors.libreoffice.enable {
+    # nl_NL = nl-nl;
+    # nl-nl = mkDictFromLibreofficeGit {
+    #   subdir = "nl";
+    #   shortName = "nl-nl";
+    #   shortDescription = "Nederlands (Nederland)";
+    #   dictFileName = "nl_NL";
+    #   readmeFileName = "nl";
+    # };
     user.packages = attrValues {
-      inherit (pkgs) libreoffice hunspell;
-      inherit (pkgs.hunspellDicts) nl_NL en_US;
+      inherit (pkgs) libreoffice hunspell; # hyphenDicts.nl_NL;
+      inherit (pkgs.hunspellDicts) en_US; #nl_NL
     };
   };
+  # };
 }
---
+++ modules/shell/toolset/mise.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.attrsets) attrValues;
+  inherit (lib.modules) mkIf;
+in {
+  options.modules.shell.mise = let
+    inherit (lib.options) mkEnableOption;
+  in {enable = mkEnableOption "mise-en-place";};
+
+  config = mkIf config.modules.shell.mise.enable {
+    user.packages = attrValues {inherit (pkgs) mise;};
+
+    # Android Debug Bridge
+    programs.mise.enable = true;
+  };
+}
