4ac53d10f3053cce1e9b7081c999b03361cd7f0e
rolfst <rolfst@gmail.com>
2025-08-07 09:59:11 +0200

    feat(power): better power management

--- hosts/vm/hardware-configuration.nix
+++ hosts/vm/hardware-configuration.nix
@@ -43,10 +43,48 @@ in {
   };
 
   powerManagement.cpuFreqGovernor = mkDefault "schedutil";
+  powerManagement.powertop.enable.true;
+  powerManagement.enable = true;
 
   # Manage device power-control:
   services = {
-    power-profiles-daemon.enable = true;
+    power-profiles-daemon.enable = false;
+    tlp = {
+        enable = true;
+        settings = {
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
+            CPU_DRIVER_OPMODE_ON_AC = "active";
+            CPU_DRIVER_OPMODE_ON_BAT = "active";
+
+            RUNTIME_PM_ON_AC = "auto";
+            RUNTIME_PM_ON_BAT = "auto";
+
+            CPU_MIN_PERF_ON_AC = "0";
+            CPU_MIN_PERF_ON_BAT = "0";
+            CPU_MAX_PERF_ON_AC = "100";
+            CPU_MAX_PERF_ON_BAT = "50";
+
+            CPU_BOOST_ON_AC = "1";
+            CPU_BOOST_ON_BAT = "0";
+
+            MEM_SLEEP_ON_AC = "deep";
+            MEM_SLEEP_ON_BAT = "deep";
+
+            PLATFORM_PROFILE_ON_AC = "performance";
+            PLATFORM_PROFILE_ON_BAT = "low-power";
+
+            NMI_WATCHDOG = 0;
+            RESTORE_DEVICE_STATE_ON_STARTUP = "1";
+
+            # Optional helps with battery longevity:
+            START_CHARGE_THRESH_BAT0 = "40";
+            STOP_CHARGE_THRESH_BAT0 = "80";
+
+        }
+    }
     thermald.enable = true;
   };
 
