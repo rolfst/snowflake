2c135915a8cdb53f59a523dd9efbdef502cbff13
rolfst <rolfst@gmail.com>
2025-11-18 10:40:28 +0100

    chore: use os delivered js-debugger since it won't install using mason anymore

--- modules/desktop/editors/neovim.nix
+++ modules/desktop/editors/neovim.nix
@@ -52,22 +52,23 @@ in {
             prompt_toolkit
             requests
           ];
-        # plugins = with pkgs.unstable.vimPlugins; [
-        #   nvim-treesitter.withAllGrammars
-        #   nvim-treesitter-parsers.css
-        #   nvim-treesitter-parsers.haskell
-        #   nvim-treesitter-parsers.javascript
-        #   nvim-treesitter-parsers.jsdoc
-        #   nvim-treesitter-parsers.json
-        #   nvim-treesitter-parsers.nix
-        #   nvim-treesitter-parsers.markdown
-        #   nvim-treesitter-parsers.python
-        #   nvim-treesitter-parsers.rust
-        #   nvim-treesitter-parsers.typescript
-        #   nvim-treesitter-parsers.tsx
-        #   nvim-treesitter-parsers.toml
-        #   nvim-treesitter-parsers.yaml
-        # ];
+        plugins = with pkgs.unstable.vimPlugins; [
+          nvim-dap-vscode-js
+          #   nvim-treesitter.withAllGrammars
+          #   nvim-treesitter-parsers.css
+          #   nvim-treesitter-parsers.haskell
+          #   nvim-treesitter-parsers.javascript
+          #   nvim-treesitter-parsers.jsdoc
+          #   nvim-treesitter-parsers.json
+          #   nvim-treesitter-parsers.nix
+          #   nvim-treesitter-parsers.markdown
+          #   nvim-treesitter-parsers.python
+          #   nvim-treesitter-parsers.rust
+          #   nvim-treesitter-parsers.typescript
+          #   nvim-treesitter-parsers.tsx
+          #   nvim-treesitter-parsers.toml
+          #   nvim-treesitter-parsers.yaml
+        ];
       };
 
       # Required API key for ChatGPT:
@@ -77,27 +78,58 @@ in {
     (mkIf cfg.rolfst.enable {
       modules.develop.lua.enable = true;
 
-      create.configFile = {
-        rolfst-config = {
-          target = "nvim";
+      create = {
+        configFile."nvim" = {
           source = "${inputs.nvim-dir}";
           recursive = true;
         };
-        # configFile."nvim/parser".source = "${pkgs.symlinkJoin {
-        #   name = "treesitter-parsers";
-        #   paths =
-        #     (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins:
-        #       with plugins; [
-        #         c
-        #         lua
-        #         query
-        #         python
-        #         vim
-        #         vimdoc
-        #         typescript
-        #       ]))
-        #     .dependencies;
-        # }}/parser";
+
+        configFile."nvim/after/plugin/dap-js.lua" = {
+          text = ''
+             local status_dap_ok, dap = pcall(require, "dap")
+             if not status_dap_ok then
+                 return
+             end
+             local status_ok, dap_vscode_js = pcall(require, "dap-vscode-js")
+             if not status_ok then
+                 return
+             end
+            for _, adapter in ipairs({
+                "pwa-node",
+                "pwa-chrome",
+                "pwa-msedge",
+                "node-terminal",
+                "pwa-extensionHost",
+            }) do
+                dap.adapters[adapter] = {
+                type = "server",
+                host = "localhost",
+                port = "''${port}",
+                executable = {
+                    command = "node",
+                    args = {
+                    "${pkgs.vscode-js-debug.outPath}" .. "/lib/node_modules/js-debug/dist/src/dapDebugServer.js",
+                    "''${port}",
+                    },
+                },
+                }
+            end
+          '';
+
+          # dap_vscode_js.setup({
+          #     node_path = "node",  -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+          #     debugger_path = "${pkgs.vscode-js-debug.outPath}", -- Path to vscode-js-debug installation.
+          #     -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+          #     port = 8123,
+          #     adapters = {
+          #     "pwa-node",
+          #     "pwa-chrome",
+          #     "pwa-msedge",
+          #     "node-terminal",
+          #     "pwa-extensionHost",
+          #     }, -- which adapters to register in nvim-dap
+          # })
+        };
       };
     })
   ];
