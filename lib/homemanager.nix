dd7c609b3cef10e1c896654c78ee4eba42717f21
rolfst <rolfst@gmail.com>
2025-12-11 14:18:39 +0100

    chore: add option to install crush (alternative for opencode)

--- lib/homemanager.nix
+++ lib/homemanager.nix
@@ -1,31 +1,33 @@
-{ inputs, lib, pkgs, ... }:
-
-let
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: let
   inherit (builtins) baseNameOf elem;
   inherit (lib) filterAttrs mkDefault nixosSystem removeSuffix;
   inherit (lib.my) mapModules mkHost;
-
 in {
-  mkHost = path:
-    attrs@{ system ? "x86_64-linux", ... }:
+  mkHost = path: attrs @ {system ? "x86_64-linux", ...}:
     nixosSystem {
       inherit system;
 
-      specialArgs = { inherit lib inputs system; };
+      specialArgs = {inherit lib inputs system;};
 
       modules = [
+        inputs.nur.modules.nixos.default
+        inputs.nur.repos.charmbracelet.modules.crush
         {
           nixpkgs.pkgs = pkgs;
           networking.hostName =
             mkDefault (removeSuffix ".nix" (baseNameOf path));
         }
-        (filterAttrs (n: v: !elem n [ "system" ]) attrs)
+        (filterAttrs (n: v: !elem n ["system"]) attrs)
         ../. # /default.nix
         (import path)
       ];
     };
 
-  mapHosts = dir:
-    attrs@{ system ? system, ... }:
+  mapHosts = dir: attrs @ {system ? system, ...}:
     mapModules dir (hostPath: mkHost hostPath attrs);
 }
