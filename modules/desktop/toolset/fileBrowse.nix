efcc11754a8c4a349d3c5e91277915471768e4ba
rolfst <rolfst@gmail.com>
2026-02-09 16:19:48 +0100

    feat(filebrowser): add archive management to thunar

--- modules/desktop/toolset/fileBrowse.nix
+++ modules/desktop/toolset/fileBrowse.nix
@@ -4,23 +4,31 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib.attrsets) attrValues optionalAttrs;
   inherit (lib.modules) mkIf mkMerge;
 
   cfg = config.modules.desktop.toolset.fileManager;
-in {
-  options.modules.desktop.toolset.fileManager = let
-    inherit (lib.options) mkEnableOption mkOption;
-    inherit (lib.types) nullOr enum;
-  in {
-    enable = mkEnableOption "A file-browser for our desktop";
-    program = mkOption {
-      type = nullOr (enum ["dolphin" "nautilus" "thunar"]);
-      default = "thunar";
-      description = "which file-browser to install";
+in
+{
+  options.modules.desktop.toolset.fileManager =
+    let
+      inherit (lib.options) mkEnableOption mkOption;
+      inherit (lib.types) nullOr enum;
+    in
+    {
+      enable = mkEnableOption "A file-browser for our desktop";
+      program = mkOption {
+        type = nullOr (enum [
+          "dolphin"
+          "nautilus"
+          "thunar"
+        ]);
+        default = "thunar";
+        description = "which file-browser to install";
+      };
     };
-  };
 
   config = mkMerge [
     {
@@ -28,7 +36,8 @@ in {
       modules.desktop.extensions.mimeApps.defApps.fileBrowser = cfg.program;
       services.gvfs.enable = true;
 
-      environment.systemPackages = attrValues ({}
+      environment.systemPackages = attrValues (
+        { }
         // optionalAttrs (cfg.program == "dolphin") {
           inherit (pkgs) dolphin dolphin-plugins;
         }
@@ -36,19 +45,22 @@ in {
           inherit (pkgs.gnome) nautilus;
         }
         // optionalAttrs (cfg.program == "thunar") {
-          inherit
-            (pkgs.xfce)
-            thunar
-            thunar-volman
-            thunar-archive-plugin
-            thunar-media-tags-plugin
-            ;
-          inherit (pkgs.mate) engrampa;
-        });
+          inherit (pkgs) file-roller;
+        }
+      );
     }
 
     (mkIf (cfg.program == "thunar") {
+      programs.xfconf.enable = true;
       services.tumbler.enable = true;
+      programs.thunar = {
+        enable = true;
+        plugins = with pkgs.xfce; [
+          thunar-volman
+          thunar-archive-plugin
+          thunar-media-tags-plugin
+        ];
+      };
 
       create.configFile = {
         "Thunar/uca.xml".text = ''
@@ -67,27 +79,27 @@ in {
           </actions>
         '';
 
-        "xfce4/xfconf/xfce-perchannel-xml/thunar.xml".text = ''
-          <?xml version="1.0" encoding="UTF-8"?>
-
-          <channel name="thunar" version="1.0">
-            <property name="last-view" type="string" value="ThunarIconView"/>
-            <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_150_PERCENT"/>
-            <property name="last-window-width" type="int" value="946"/>
-            <property name="last-window-height" type="int" value="503"/>
-            <property name="last-window-maximized" type="bool" value="false"/>
-            <property name="last-side-pane" type="string" value="ThunarTreePane"/>
-            <property name="last-separator-position" type="int" value="213"/>
-            <property name="last-location-bar" type="string" value="ThunarLocationEntry"/>
-            <property name="misc-single-click" type="bool" value="false"/>
-            <property name="misc-thumbnail-mode" type="string" value="THUNAR_THUMBNAIL_MODE_ALWAYS"/>
-            <property name="misc-text-beside-icons" type="bool" value="false"/>
-            <property name="misc-date-style" type="string" value="THUNAR_DATE_STYLE_SHORT"/>
-            <property name="shortcuts-icon-size" type="string" value="THUNAR_ICON_SIZE_32"/>
-            <property name="tree-icon-size" type="string" value="THUNAR_ICON_SIZE_24"/>
-            <property name="last-menubar-visible" type="bool" value="false"/>
-          </channel>
-        '';
+        # "xfce4/xfconf/xfce-perchannel-xml/thunar.xml".text = ''
+        #   <?xml version="1.0" encoding="UTF-8"?>
+        #
+        #   <channel name="thunar" version="1.0">
+        #     <property name="last-view" type="string" value="ThunarListView"/>
+        #     <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_150_PERCENT"/>
+        #     <property name="last-window-width" type="int" value="946"/>
+        #     <property name="last-window-height" type="int" value="503"/>
+        #     <property name="last-window-maximized" type="bool" value="false"/>
+        #     <property name="last-side-pane" type="string" value="ThunarTreePane"/>
+        #     <property name="last-separator-position" type="int" value="213"/>
+        #     <property name="last-location-bar" type="string" value="ThunarLocationEntry"/>
+        #     <property name="misc-single-click" type="bool" value="false"/>
+        #     <property name="misc-thumbnail-mode" type="string" value="THUNAR_THUMBNAIL_MODE_ALWAYS"/>
+        #     <property name="misc-text-beside-icons" type="bool" value="false"/>
+        #     <property name="misc-date-style" type="string" value="THUNAR_DATE_STYLE_SHORT"/>
+        #     <property name="shortcuts-icon-size" type="string" value="THUNAR_ICON_SIZE_32"/>
+        #     <property name="tree-icon-size" type="string" value="THUNAR_ICON_SIZE_24"/>
+        #     <property name="last-menubar-visible" type="bool" value="false"/>
+        #   </channel>
+        # '';
       };
     })
   ];
