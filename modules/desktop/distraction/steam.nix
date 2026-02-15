c88e4d98aaa0b00e4e1ae77d946aebe8918e8e5d
rolfst <rolfst@gmail.com>
2026-02-15 14:09:52 +0100

    chore(steam): work on wayland

--- modules/desktop/distraction/steam.nix
+++ modules/desktop/distraction/steam.nix
@@ -4,67 +4,86 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib.meta) getExe;
   inherit (lib.modules) mkIf mkMerge;
   cfg = config.modules.desktop.distraction.steam;
-in {
-  options.modules.desktop.distraction.steam = let
-    inherit (lib.options) mkEnableOption;
-    inherit (lib.types) str;
-    inherit (lib.my) mkOpt;
-  in {
-    enable = mkEnableOption "game/software store";
-    hardware.enable = mkEnableOption "Steam-based HW support";
-    libDir = mkOpt str "$XDG_DATA_HOME/steamlib";
-  };
+in
+{
+  options.modules.desktop.distraction.steam =
+    let
+      inherit (lib.options) mkEnableOption;
+      inherit (lib.types) str;
+      inherit (lib.my) mkOpt;
+    in
+    {
+      enable = mkEnableOption "game/software store";
+      hardware.enable = mkEnableOption "Steam-based HW support";
+      libDir = mkOpt str "$XDG_DATA_HOME/steamlib";
+    };
 
   config = mkIf cfg.enable (mkMerge [
     {
+      # when running steam on x11 do this
       # I avoid programs.steam.enable because it installs another steam binary,
       # which the xdesktop package invokes, instead of my steam shims below.
-      user.packages = let
-        inherit
-          (pkgs)
-          makeDesktopItem
-          stdenv
-          steam
-          steam-run
-          writeScriptBin
-          ;
-      in [
-        # Get steam to keep its garbage out of $HOME
-        (writeScriptBin "steam" ''
-          #!${stdenv.shell}
-          HOME="${cfg.libDir}" exec ${getExe steam} "$@"
-        '')
+      # user.packages = let
+      #   inherit
+      #     (pkgs)
+      #     makeDesktopItem
+      #     stdenv
+      #     steam
+      #     steam-run
+      #     writeScriptBin
+      #     ;
+      # in [
+      #   # Get steam to keep its garbage out of $HOME
+      #   (writeScriptBin "steam" ''
+      #     #!${stdenv.shell}
+      #     HOME="${cfg.libDir}" exec ${getExe steam} "$@"
+      #   '')
+      #
+      #   # for running GOG and humble bundle games
+      #   (writeScriptBin "steam-run" ''
+      #     #!${stdenv.shell}
+      #     HOME="${cfg.libDir}" exec ${getExe steam-run} "$@"
+      #   '')
+      #
+      # Add rofi desktop icon
+      #   (makeDesktopItem {
+      #     name = "steam";
+      #     desktopName = "Steam";
+      #     icon = "steam";
+      #     exec = "steam";
+      #     terminal = false;
+      #     mimeTypes = ["x-scheme-handler/steam"];
+      #     categories = ["Network" "FileTransfer" "Game"];
+      #   })
+      # ];
 
-        # for running GOG and humble bundle games
-        (writeScriptBin "steam-run" ''
-          #!${stdenv.shell}
-          HOME="${cfg.libDir}" exec ${getExe steam-run} "$@"
-        '')
+      # system.userActivationScripts.setupSteamDir = ''
+      #   mkdir -p "${cfg.libDir}"
+      # '';
 
-        # Add rofi desktop icon
-        (makeDesktopItem {
-          name = "steam";
-          desktopName = "Steam";
-          icon = "steam";
-          exec = "steam";
-          terminal = false;
-          mimeTypes = ["x-scheme-handler/steam"];
-          categories = ["Network" "FileTransfer" "Game"];
-        })
+      # for running steam under wayland do this:
+      programs = {
+        steam.enable = true;
+        steam.gamescopeSession.enable = true;
+        gamemode.enable = true;
+      };
+      environment.systemPackages = with pkgs; [
+        mangohud
+        protonup-qt
+        lutris
+        bottles
+        heroic
       ];
-
-      system.userActivationScripts.setupSteamDir = ''
-        mkdir -p "${cfg.libDir}"
-      '';
 
       # better for steam proton games
       # systemd.extraConfig = "DefaultLimitNOFILE=1048576";
     }
 
-    (mkIf cfg.hardware.enable {hardware.steam-hardware.enable = true;})
+    (mkIf cfg.hardware.enable { hardware.steam-hardware.enable = true; })
   ]);
 }
