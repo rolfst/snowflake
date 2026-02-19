3328d94c1b2ef729fc1865a8db1c5ca3ffe5edd2
rolfst <rolfst@gmail.com>
2026-02-19 12:36:04 +0100

    chore(power management): allow suspension when lid is closed

--- modules/hardware/laptop.nix
+++ modules/hardware/laptop.nix
@@ -4,32 +4,41 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   inherit (lib.modules) mkIf mkMerge;
   inherit (lib.attrsets) attrValues;
   cfg = config.modules.hardware.laptop;
-in {
-  options.modules.hardware.laptop = let
-    inherit (lib.options) mkEnableOption;
-  in {
-    enable = mkEnableOption "laptop support";
-  };
+in
+{
+  options.modules.hardware.laptop =
+    let
+      inherit (lib.options) mkEnableOption;
+    in
+    {
+      enable = mkEnableOption "laptop support";
+    };
 
   config = mkMerge [
-    (
-      mkIf cfg.enable {
-        services.auto-cpufreq.enable = true;
-        services.auto-cpufreq.settings = {
-          battery = {
-            governor = "powersave";
-            turbo = "never";
-          };
-          charger = {
-            governor = "performance";
-            turbo = "auto";
-          };
+    (mkIf cfg.enable {
+      services.logind = {
+        settings.Login = {
+          HandleLidSwitch = "suspend";
+          HandleLidSwitchExternalPower = "suspend";
+          HandleLidSwitchDocked = "suspend";
         };
-      }
-    )
+      };
+      services.auto-cpufreq.enable = true;
+      services.auto-cpufreq.settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    })
   ];
 }
