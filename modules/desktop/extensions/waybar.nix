93a47ab262196fecaadcf0e3850ce4129d03a921
rolfst <rolfst@gmail.com>
2026-03-02 14:33:43 +0100

    style: format

--- modules/desktop/extensions/waybar.nix
+++ modules/desktop/extensions/waybar.nix
@@ -1,12 +1,16 @@
-{ options, config, lib, pkgs, ...
-
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
 }:
-
 let
   inherit (lib) mkIf mkEnableOption;
 
   cfg = config.modules.desktop.extensions.waybar;
-in {
+in
+{
   options.modules.desktop.extensions.waybar = {
     enable = mkEnableOption "status-bar for wayland";
   };
@@ -16,113 +20,175 @@ in {
     hm.services.status-notifier-watcher.enable = true;
 
     hm.programs.waybar = {
-      enable = true;
-      settings = [{
-        "layer" = "top";
-        "position" = "top";
-        modules-left = [
-          "custom/launcher"
-          "wlr/workspaces"
-          "temperature"
-          "idle_inhibitor"
-          "mpd"
-        ];
-        modules-center = [ "clock" ];
-        modules-right =
-          [ "memory" "cpu" "network" "battery" "custom/powermenu" "tray" ];
-        "custom/launcher" = {
-          "format" = " ";
-          "on-click" = "rofi -no-lazy-grab -show drun -modi drun";
-          "tooltip" = false;
-        };
+      enable = false;
+      settings = [
+        {
+          "layer" = "top";
+          "position" = "top";
+          modules-left = [
+            "custom/launcher"
+            "wlr/workspaces"
+            "temperature"
+            "idle_inhibitor"
+            "mpd"
+          ];
+          modules-center = [ "clock" ];
+          modules-right = [
+            "custom/noctalia-recorder"
+            "custom/noctalia-clipper"
+            "custom/noctalia-notes"
+            "custom/noctalia-todo"
+            "custom/noctalia-cheatsheet"
+            "custom/noctalia-assistant"
+            "memory"
+            "cpu"
+            "network"
+            "battery"
+            "custom/powermenu"
+            "tray"
+          ];
+          "custom/launcher" = {
+            "format" = " ";
+            "on-click" = "rofi -no-lazy-grab -show drun -modi drun";
+            "tooltip" = false;
+          };
 
-        "wlr/workspaces" = {
-          "format" = "{icon}";
-          "on-click" = "activate";
-          "on-scroll-up" = "hyprctl dispatch workspace e+1";
-          "on-scroll-down" = "hyprctl dispatch workspace e-1";
-        };
-        "idle_inhibitor" = {
-          "format" = "{icon}";
-          "format-icons" = {
-            "activated" = "";
-            "deactivated" = "";
+          "custom/noctalia-recorder" = {
+            "format" = " ";
+            "on-click" = "noctalia-shell ipc call plugin:screen-recorder toggle";
+            "tooltip" = true;
+            "tooltip-format" = "Screen Recorder";
           };
-          "tooltip" = false;
-        };
-        "battery" = {
-          "interval" = 10;
-          "states" = {
-            "warning" = 20;
-            "critical" = 10;
+          "custom/noctalia-clipper" = {
+            "format" = " ";
+            "on-click" = "noctalia-shell ipc call plugin:clipper toggle";
+            "tooltip" = true;
+            "tooltip-format" = "Clipboard Manager";
           };
-          "format" = "{icon} {capacity}%";
-          "format-icons" = [ "" "" "" "" "" "" "" "" "" ];
-          "format-full" = "{icon} {capacity}%";
-          "format-charging" = " {capacity}%";
-          "tooltip" = false;
-        };
-        "clock" = {
-          "on-click" = "wallpaper_random";
-          "on-click-right" = "killall dynamic_wallpaper || dynamic_wallpaper &";
-          "interval" = 1;
-          "format" = "{:%I:%M %p  %A %b %d}";
-          "tooltip" = true;
-          # "tooltip-format"= "{=%A; %d %B %Y}\n<tt>{calendar}</tt>"
-          "tooltip-format" = ''
-            上午：高数
-            下午：Ps
-            晚上：Golang
-            <tt>{calendar}</tt>'';
-        };
-        "memory" = {
-          "interval" = 1;
-          "format" = "﬙ {percentage}%";
-          "states" = { "warning" = 85; };
-        };
-        "cpu" = {
-          "interval" = 1;
-          "format" = " {usage}%";
-        };
-        "mpd" = {
-          "max-length" = 25;
-          "format" = "<span foreground='#bb9af7'></span> {title}";
-          "format-paused" = " {title}";
-          "format-stopped" = "<span foreground='#bb9af7'></span>";
-          "format-disconnected" = "";
-          "on-click" = "mpc --quiet toggle";
-          "on-click-right" = "mpc ls | mpc add";
-          "on-click-middle" = "kitty --class='ncmpcpp' --hold sh -c 'ncmpcpp'";
-          "on-scroll-up" = "mpc --quiet prev";
-          "on-scroll-down" = "mpc --quiet next";
-          "smooth-scrolling-threshold" = 5;
-          "tooltip-format" =
-            "{title} - {artist} ({elapsedTime:%M:%S}/{totalTime:%H:%M:%S})";
-        };
-        "network" = {
-          "interval" = 1;
-          "format-wifi" = "說 {essid}";
-          "format-ethernet" = "  {ifname} ({ipaddr})";
-          "format-linked" = "說 {essid} (No IP)";
-          "format-disconnected" = "說 Disconnected";
-          "tooltip" = false;
-        };
-        "temperature" = {
-          # "hwmon-path"= "${env:HWMON_PATH}";
-          #"critical-threshold"= 80;
-          "tooltip" = false;
-          "format" = " {temperatureC}°C";
-        };
-        "custom/powermenu" = {
-          "format" = "";
-          "on-click" = ""; # TODO
-          "tooltip" = false;
-        };
-        "tray" = {
-          "icon-size" = 15;
-          "spacing" = 5;
-        };
-      }];
+          "custom/noctalia-notes" = {
+            "format" = " ";
+            "on-click" = "noctalia-shell ipc call plugin:notes-scratchpad toggle";
+            "tooltip" = true;
+            "tooltip-format" = "Notes";
+          };
+          "custom/noctalia-todo" = {
+            "format" = " ";
+            "on-click" = "noctalia-shell ipc call plugin:todo toggle";
+            "tooltip" = true;
+            "tooltip-format" = "Todo List";
+          };
+          "custom/noctalia-cheatsheet" = {
+            "format" = " ";
+            "on-click" = "noctalia-shell ipc call plugin:keybind-cheatsheet toggle";
+            "tooltip" = true;
+            "tooltip-format" = "Keybindings";
+          };
+          "custom/noctalia-assistant" = {
+            "format" = " ";
+            "on-click" = "noctalia-shell ipc call plugin:assistant-panel toggle";
+            "tooltip" = true;
+            "tooltip-format" = "Assistant";
+          };
+
+          "wlr/workspaces" = {
+            "format" = "{icon}";
+            "on-click" = "activate";
+            "on-scroll-up" = "hyprctl dispatch workspace e+1";
+            "on-scroll-down" = "hyprctl dispatch workspace e-1";
+          };
+          "idle_inhibitor" = {
+            "format" = "{icon}";
+            "format-icons" = {
+              "activated" = "";
+              "deactivated" = "";
+            };
+            "tooltip" = false;
+          };
+          "battery" = {
+            "interval" = 10;
+            "states" = {
+              "warning" = 20;
+              "critical" = 10;
+            };
+            "format" = "{icon} {capacity}%";
+            "format-icons" = [
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+            ];
+            "format-full" = "{icon} {capacity}%";
+            "format-charging" = " {capacity}%";
+            "tooltip" = false;
+          };
+          "clock" = {
+            "on-click" = "wallpaper_random";
+            "on-click-right" = "killall dynamic_wallpaper || dynamic_wallpaper &";
+            "interval" = 1;
+            "format" = "{:%I:%M %p  %A %b %d}";
+            "tooltip" = true;
+            # "tooltip-format"= "{=%A; %d %B %Y}\n<tt>{calendar}</tt>"
+            "tooltip-format" = ''
+              上午：高数
+              下午：Ps
+              晚上：Golang
+              <tt>{calendar}</tt>'';
+          };
+          "memory" = {
+            "interval" = 1;
+            "format" = "﬙ {percentage}%";
+            "states" = {
+              "warning" = 85;
+            };
+          };
+          "cpu" = {
+            "interval" = 1;
+            "format" = " {usage}%";
+          };
+          "mpd" = {
+            "max-length" = 25;
+            "format" = "<span foreground='#bb9af7'></span> {title}";
+            "format-paused" = " {title}";
+            "format-stopped" = "<span foreground='#bb9af7'></span>";
+            "format-disconnected" = "";
+            "on-click" = "mpc --quiet toggle";
+            "on-click-right" = "mpc ls | mpc add";
+            "on-click-middle" = "kitty --class='ncmpcpp' --hold sh -c 'ncmpcpp'";
+            "on-scroll-up" = "mpc --quiet prev";
+            "on-scroll-down" = "mpc --quiet next";
+            "smooth-scrolling-threshold" = 5;
+            "tooltip-format" = "{title} - {artist} ({elapsedTime:%M:%S}/{totalTime:%H:%M:%S})";
+          };
+          "network" = {
+            "interval" = 1;
+            "format-wifi" = "說 {essid}";
+            "format-ethernet" = "  {ifname} ({ipaddr})";
+            "format-linked" = "說 {essid} (No IP)";
+            "format-disconnected" = "說 Disconnected";
+            "tooltip" = false;
+          };
+          "temperature" = {
+            # "hwmon-path"= "${env:HWMON_PATH}";
+            #"critical-threshold"= 80;
+            "tooltip" = false;
+            "format" = " {temperatureC}°C";
+          };
+          "custom/powermenu" = {
+            "format" = "";
+            "on-click" = ""; # TODO
+            "tooltip" = false;
+          };
+          "tray" = {
+            "icon-size" = 15;
+            "spacing" = 5;
+          };
+        }
+      ];
       style = ''
               * {
                 font-family: "JetBrainsMono Nerd Font";
