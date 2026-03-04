7c969e9dfd6e8eb7284837466b123bac05191e9c
rolfst <rolfst@gmail.com>
2026-03-04 15:43:38 +0100

    chore(ai): remove opencode from installation

--- modules/shell/toolset/ai.nix
+++ modules/shell/toolset/ai.nix
@@ -5,15 +5,21 @@
   pkgs,
   inputs,
   ...
-}: let
+}:
+let
   inherit (lib.attrsets) attrValues optionalAttrs;
   inherit (lib.modules) mkIf;
-in {
-  options.modules.shell.toolset.AI = let
-    inherit (lib.options) mkEnableOption;
-  in {enable = mkEnableOption "Agentic AI code base helper";};
+in
+{
+  options.modules.shell.toolset.AI =
+    let
+      inherit (lib.options) mkEnableOption;
+    in
+    {
+      enable = mkEnableOption "Agentic AI code base helper";
+    };
 
   config = mkIf config.modules.shell.toolset.AI.enable {
-    environment.systemPackages = with pkgs; [opencode];
+    environment.systemPackages = with pkgs; [ ];
   };
 }
