da5437e743ab21941f882dccc069f08c997dc38f
rolfst <rolfst@gmail.com>
2026-06-15 14:28:45 +0200

    fix(citrix): register MIME type, receiver:// URL scheme, and expose ctxwebhelper

--- modules/desktop/toolset/citrix.nix
+++ modules/desktop/toolset/citrix.nix
@@ -21,8 +21,29 @@ in
   };
   config = mkMerge [
     (mkIf cfg.enable {
-      environment.systemPackages = attrValues {
-        inherit (pkgs.unstable) citrix_workspace;
+      # The package ships Citrix-mime_types.xml in share/applications/
+      # but shared-mime-info expects it in share/mime/packages/.
+      # Also, ctxwebhelper (receiver:// URL handler) is not exposed in bin/.
+      environment.systemPackages = let
+        citrix = pkgs.unstable.citrix_workspace;
+        citrixMime = pkgs.runCommand "citrix-mime" {} ''
+          mkdir -p $out/share/mime/packages
+          cp ${citrix}/share/applications/Citrix-mime_types.xml \
+             $out/share/mime/packages/
+        '';
+        citrixWebHelper = pkgs.writeShellScriptBin "ctxwebhelper" ''
+          exec ${citrix}/opt/citrix-icaclient/util/ctxwebhelper "$@"
+        '';
+      in
+        (attrValues {
+          inherit (pkgs.unstable) citrix_workspace;
+        })
+        ++ [citrixMime citrixWebHelper];
+
+      # Register handlers for ICA files and Citrix URL schemes
+      hm.xdg.mimeApps.defaultApplications = {
+        "application/x-ica" = "wfica.desktop";
+        "x-scheme-handler/receiver" = "receiver.desktop";
       };
     })
   ];
